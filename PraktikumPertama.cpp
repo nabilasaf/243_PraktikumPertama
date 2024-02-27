c1c497051ae5a89dc9d47ddc7f8a0dd644d37705
nabilasaf <nabilasafitri861@gmail.com>
2024-02-27 15:31:15 +0700

    Add project files.

---
+++ PraktikumPertama.cpp
@@ -0,0 +1,27 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //begin 
+    //numeric nAlas, nTinggi, nLuas
+    //display 'Masukan Alas = '
+    //Accept nAlas
+    //Display 'Masukan Tinggi = '
+    //Accept nTinggi
+    //Compute nLuas = nAlas*nTinggi/2
+    //Display 'Luasnya = ' + nLuas
+    //End
+
+    float nAlas, nTinggi, nLuas;
+    cout << "Masukan Alas = ";
+    cin >> nAlas;
+    cout << "Masukan Tinggi = ";
+    cin >> nTinggi;
+    nLuas = nAlas * nTinggi   / 2;
+    cout << "Luasnya = " << nLuas;
+
+
+}
+
